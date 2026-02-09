5f1c68634dd0e3365b71de8a750b2b292bdf15b3
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-02-09 22:41:32 +0530

    feat: Customer Controller

--- src/com/stschool/ecommerce/controller/CustomerController.java
+++ src/com/stschool/ecommerce/controller/CustomerController.java
@@ -1,5 +1,12 @@
 package com.stschool.ecommerce.controller;
 
+import com.stchool.ecommerce.model.Customer;
+
 public class CustomerController {
+     public void handlingSignUp(Customer customer)
+     {
+         System.out.println("Sign up Successful" +customer);
+     }
+
 
 }
