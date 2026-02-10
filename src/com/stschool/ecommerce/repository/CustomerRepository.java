b9200d176b955b40d123415155568939a54d9a75
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-02-10 22:27:35 +0530

    feat: Customer Repository layer  that which stores  customer data

--- src/com/stschool/ecommerce/repository/CustomerRepository.java
+++ src/com/stschool/ecommerce/repository/CustomerRepository.java
@@ -5,7 +5,6 @@ import com.stchool.ecommerce.model.Customer;
 public class CustomerRepository {
       public Customer addNewCustomer ( Customer customer){
           System.out.println(" Added New Customer :");
-          System.out.println("Customer details:" + customer);
           return  customer;
 
       }
