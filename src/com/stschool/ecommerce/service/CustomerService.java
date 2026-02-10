96a3a225d30ae8d46d7f3a8edf9134fb2b9eca37
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-02-10 21:53:43 +0530

    feat: Customer Service layer that which transfers  valid data from Controller  Layer to Repository Layer

--- src/com/stschool/ecommerce/service/CustomerService.java
+++ src/com/stschool/ecommerce/service/CustomerService.java
@@ -1,5 +1,24 @@
 package com.stschool.ecommerce.service;
 
+import com.stchool.ecommerce.model.Customer;
+import com.stschool.ecommerce.repository.CustomerRepository;
+
 public class CustomerService {
+        public Customer handleSignUp(Customer customer){
+            System.out.println("Customer Service:" + customer);
+            CustomerRepository customerRepository = new CustomerRepository();
+            return customerRepository.addNewCustomer(customer);
+
+
+        }
+    public void displayCustomerDetails(Customer customer)
+    {
+        System.out.println("Customer Details:" + customer);
+        System.out.println("Customer FirstName:" + customer.getFirstName());
+        System.out.println("Customer lastName:" + customer.getLastName());
+        System.out.println("Customer  Email: " + customer.getEmail());
+
+
+    }
 
 }
