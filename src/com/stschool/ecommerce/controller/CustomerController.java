95232843f5920d05ba60633b3e36c0be368d596d
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-02-10 21:49:09 +0530

    feat: Customer Controller layer that checks validation of data

--- src/com/stschool/ecommerce/controller/CustomerController.java
+++ src/com/stschool/ecommerce/controller/CustomerController.java
@@ -1,12 +1,16 @@
 package com.stschool.ecommerce.controller;
 
 import com.stchool.ecommerce.model.Customer;
+import com.stschool.ecommerce.service.CustomerService;
 
 public class CustomerController {
-     public void handlingSignUp(Customer customer)
+     public Customer handleSignUp(Customer customer)
      {
          System.out.println("Sign up Successful" +customer);
+           CustomerService customerService = new CustomerService();
+          return customerService.handleSignUp(customer);
+
+
+
      }
-
-
 }
